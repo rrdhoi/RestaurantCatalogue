0ae1dccedfc592e38b4321d05fdc0ee58427efeb
rrdhoi <r.ridoi702@gmail.com>
2021-05-23 09:02:31 +0800

    fix encryption, obfuscation, and certificate pinning

--- core/src/main/java/com/bikincerdas/core/di/CoreModule.kt
+++ core/src/main/java/com/bikincerdas/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.bikincerdas.core.data.source.remote.RemoteDataSource
 import com.bikincerdas.core.data.source.remote.network.ApiService
 import com.bikincerdas.core.domain.repository.IRestaurantRepository
 import com.bikincerdas.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,31 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<RestaurantDatabase>().restaurantDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             RestaurantDatabase::class.java, "Restaurant.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "restaurant-api.dicoding.dev"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/CMznK6TSMddNnaTu0qeGz4dp5SNI7YO99qwBmew+PrM=")
+            .add(hostname, "sha256/cXjPgKdVe6iojP8s0YQJ3rtmDFHTnYZxcYvmYGFiYME=")
+            .add(hostname, "sha256/hxqRlPTu1bMS/0DITB1SSu0vd4u/8l8TjPgfaAp63Gc=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
